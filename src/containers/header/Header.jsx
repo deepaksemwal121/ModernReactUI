4093316a6407234b17bf9be524638ebdb6a5e2ff
deepaksemwal121 <deepaksemwal121@gmail.com>
2021-11-19 20:49:54 +0530

    Created the landing page till CTA section

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
-  return <div className='gpt3__header section__padding' id="home">
-
-  </div>;
+  return (
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's Build Something amzing with GPT-3 OpenAI
+        </h1>
+        <p>
+          GPT-3’s ability to identify themes from natural language and generate
+          summaries allows Viable to give product, customer experience, and
+          marketing teams at companies across industries a better understanding
+          of their customers’ wants and needs,” said Daniel Erickson, CEO of
+          Viable.
+        </p>
+        <div className="gpt3__header-content__input">
+          <input
+            type="email"
+            name="email"
+            id=""
+            placeholder="Your Email Address"
+          />
+          <button type="button"> Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people using gpt3" />
+          <p>1000 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="AIimage" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,6 +1,26 @@
 import React from "react";
 import "./possibility.css";
+import posb from "../../assets/possibility.png";
 const Possibility = () => {
-  return <div></div>;
+  return (
+    <div className="gpt3__possibility section__padding">
+      <div className="gpt3__possibility-image">
+        <img src={posb} alt="" srcset="" />
+      </div>
+      <div className="gpt3__possibility-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>
+          We so opinion friends me message as delight. Whole front do of plate
+          heard oh ought. His defective nor convinced residence own. Connection
+          has put impossible own apartments boisterous. At jointure ladyship an
+          insisted so humanity he. Friendly bachelor entrance to on by
+        </p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+    </div>
+  );
 };
 export default Possibility;
